b63cc3b4de728d75a0fe1519e26452d330ce4c98
Panitan Chaoum <panitan.cha@ascendcorp.com>
2019-06-10 04:11:21 +0000

    Updates

---
+++ precache-manifest.e77ac09ae39edc4cdf4e3d1dacb5bb14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a9062a4dec81d75f2ac8b6d4f81a1d8",
+    "url": "/knowledge-sharing/index.html"
+  },
+  {
+    "revision": "e029cce56cebbbbe1c72",
+    "url": "/knowledge-sharing/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/knowledge-sharing/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "e029cce56cebbbbe1c72",
+    "url": "/knowledge-sharing/static/js/main.ae7424b1.chunk.js"
+  },
+  {
+    "revision": "bbb548de462378466ff6",
+    "url": "/knowledge-sharing/static/js/runtime~main.8a8712a7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/knowledge-sharing/static/media/logo.5d5d9eef.svg"
+  }
+]);
